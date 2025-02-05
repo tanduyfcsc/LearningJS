1bf3c10f6422774cd1af33b787881175c3404659
tanduyfcsc <tanduyfcsc@gmail.com>
2025-02-05 18:24:24 +0700

    1.17. JS Object Properties

--- TestJS.js
+++ TestJS.js
@@ -3,7 +3,13 @@ const person = {
     lastName: "Doe",
     age: 20,
     eyeColor: "blue",
+    myCars: {car1:"Ford", car2:"BWM", car3:"Fiat"},
     fullName: function() {return this.firstName + " " + this.lastName},
 };
 
-document.getElementById("demo").innerHTML = person.firstName + " is " + person["age"] + " years old" + " and " + person.fullName();
+let c1 = "myCars";
+let c2 = "car3";
+
+// delete person.age
+
+document.getElementById("demo").innerHTML = person.firstName + " is " + typeof person["age"] + " years old" + " and " + person.fullName() + " car name: " + person[c1][c2];
