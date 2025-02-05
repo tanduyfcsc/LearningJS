4b772912f826a5f555faf8b09e926920193eac22
tanduyfcsc <tanduyfcsc@gmail.com>
2025-02-05 18:38:02 +0700

    1.18. JS Object Methods

--- TestJS.js
+++ TestJS.js
@@ -4,12 +4,16 @@ const person = {
     age: 20,
     eyeColor: "blue",
     myCars: {car1:"Ford", car2:"BWM", car3:"Fiat"},
-    fullName: function() {return this.firstName + " " + this.lastName},
+    // fullName: function() {return this.firstName + " " + this.lastName},
 };
 
-let c1 = "myCars";
-let c2 = "car3";
+// let c1 = "myCars";
+// let c2 = "car3";
 
 // delete person.age
 
-document.getElementById("demo").innerHTML = person.firstName + " is " + typeof person["age"] + " years old" + " and " + person.fullName() + " car name: " + person[c1][c2];
+person.fullName = function() {
+    return (this.firstName + " " + this.lastName).toUpperCase();
+}
+
+document.getElementById("demo").innerHTML = person.firstName + " is " + person["age"] + " years old" + " and " + person.fullName();
