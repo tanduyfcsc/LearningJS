39337a08c96f25eb15ca21366ca7ac3bf49575a0
tanduyfcsc <tanduyfcsc@gmail.com>
2025-02-06 09:29:25 +0700

    1.19. JS Display

--- TestJS.js
+++ TestJS.js
@@ -3,7 +3,7 @@ const person = {
     lastName: "Doe",
     age: 20,
     eyeColor: "blue",
-    myCars: {car1:"Ford", car2:"BWM", car3:"Fiat"},
+    // myCars: {car1:"Ford", car2:"BWM", car3:"Fiat"},
     // fullName: function() {return this.firstName + " " + this.lastName},
 };
 
@@ -12,8 +12,25 @@ const person = {
 
 // delete person.age
 
-person.fullName = function() {
-    return (this.firstName + " " + this.lastName).toUpperCase();
+// person.fullName = function() {
+//     return (this.firstName + " " + this.lastName).toUpperCase();
+// }
+
+// let text = "";
+// for (let x in person) {
+//     text += person[x] + " ";
+// }
+
+// const myArr = Object.values(person);
+
+// document.getElementById("demo").innerHTML = myArr;
+
+const fruits = {Bananas: 300, Oranges: 200, Apples: 500};
+
+let text = "";
+
+for (let [fruit, amount] of Object.entries(fruits)){
+    text += fruit + " " + amount + "<br>";
 }
 
-document.getElementById("demo").innerHTML = person.firstName + " is " + person["age"] + " years old" + " and " + person.fullName();
+document.getElementById("demo").innerHTML = JSON.stringify(person);
