ec3152c7493de626cf052ad6259db2d27ab19ff2
tanduyfcsc <tanduyfcsc@gmail.com>
2025-02-06 11:38:38 +0700

    1.20. JS Object Constructors

--- TestJS.js
+++ TestJS.js
@@ -1,11 +1,11 @@
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 20,
-    eyeColor: "blue",
-    // myCars: {car1:"Ford", car2:"BWM", car3:"Fiat"},
-    // fullName: function() {return this.firstName + " " + this.lastName},
-};
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 20,
+//     eyeColor: "blue",
+//     // myCars: {car1:"Ford", car2:"BWM", car3:"Fiat"},
+//     // fullName: function() {return this.firstName + " " + this.lastName},
+// };
 
 // let c1 = "myCars";
 // let c2 = "car3";
@@ -25,12 +25,38 @@ const person = {
 
 // document.getElementById("demo").innerHTML = myArr;
 
-const fruits = {Bananas: 300, Oranges: 200, Apples: 500};
+// const fruits = {Bananas: 300, Oranges: 200, Apples: 500};
 
-let text = "";
+// let text = "";
 
-for (let [fruit, amount] of Object.entries(fruits)){
-    text += fruit + " " + amount + "<br>";
+// for (let [fruit, amount] of Object.entries(fruits)){
+//     text += fruit + " " + amount + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = JSON.stringify(person);
+
+function Person(first, last, age, eye){
+    this.first = first;
+    this.last = last;
+    this.age = age;
+    this.eye;
+    // this.nationality = "English"
+    this.fullName = function(){
+        return this.first + " " + this.last;
+    }
 }
 
-document.getElementById("demo").innerHTML = JSON.stringify(person);
+// const myFather = new Person("John", "Doe", 50, "Blue");
+// myFather.nationality = "English";
+
+// const myMother = new Person("Sally", "Rally", 40, "Green")
+
+// Person.prototype.nationality = "English";
+
+// Person.prototype.changeName = function(name){
+//     this.last = name;
+// }
+
+// myMother.changeName("Doe");
+
+// document.getElementById("demo").innerHTML = "My father is " + myFather.fullName() + "." + " My mother is " + " " + myMother.last + ".";
